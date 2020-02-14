911297fe1382a56875c10e11d7695f673bfbfbf4
tonyamelnychuk <antonina.mykhailenko@gmail.com>
2020-02-14 12:41:04 +0200

    try junit5

--- src/test/java/MyFirstJunit5Tests.java
+++ src/test/java/MyFirstJunit5Tests.java
@@ -1,7 +1,17 @@
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Month;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyFirstJunit5Tests {
 
@@ -14,4 +24,39 @@ public class MyFirstJunit5Tests {
     void testNumbers2() {
         assertEquals(2, 1);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 6})
+    void testNumbers3(int number) {
+        assertEquals(5, number);
+    }
+
+    @Test
+    void reportSingleValue(TestReporter testReporter) {
+        testReporter.publishEntry("a status message");
+    }
+
+    @Test
+    void reportKeyValuePair(TestReporter testReporter) {
+        testReporter.publishEntry("a key", "a value");
+    }
+
+    @Test
+    void reportMultipleKeyValuePairs(TestReporter testReporter) {
+        Map<String, String> values = new HashMap<>();
+        values.put("user name", "dk38");
+        values.put("award year", "1974");
+
+        testReporter.publishEntry(values);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Month.class, names = ".+BER", mode = EnumSource.Mode.MATCH_ANY)
+    void fourMonths_AreEndingWithBer(Month month) {
+        EnumSet<Month> months =
+                EnumSet.of(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+        assertTrue(months.contains(month));
+    }
+
+
 }
